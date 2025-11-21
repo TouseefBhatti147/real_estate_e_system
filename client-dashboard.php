3ee66b1b3fc5355930a1831121ac415480bca164
Arif Hussain <arif.hussain2014@gmail.com>
2025-11-21 20:22:50 +0000

    client pages added

--- client-dashboard.php
+++ client-dashboard.php
@@ -68,97 +68,53 @@
     <!-- /.col -->
 
 
-    <div class="col-md-12">
-        <div class="card mb-4">
-            <div class="card-header">
-                <h3 class="card-title">Simple Full Width Table</h3>
 
-                <div class="card-tools">
-                    <ul class="pagination pagination-sm float-end">
-                        <li class="page-item">
-                            <a class="page-link" href="#">«</a>
-                        </li>
-                        <li class="page-item">
-                            <a class="page-link" href="#">1</a>
-                        </li>
-                        <li class="page-item">
-                            <a class="page-link" href="#">2</a>
-                        </li>
-                        <li class="page-item">
-                            <a class="page-link" href="#">3</a>
-                        </li>
-                        <li class="page-item">
-                            <a class="page-link" href="#">»</a>
-                        </li>
-                    </ul>
-                </div>
+
+    <div class="col-md-12">
+        <div class="card mb-6">
+            <div class="card-header">
+                <h3 class="card-title">Striped Full Width Table</h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body p-0">
-                <table class="table" role="table">
+                <table class="table table-striped">
                     <thead>
                         <tr>
-                            <th style="width: 10px" scope="col">#</th>
-                            <th scope="col">Task</th>
-                            <th scope="col">Progress</th>
-                            <th style="width: 40px" scope="col">Label</th>
+                            <th style="width: 10px">Sr#</th>
+                            <th>Project Name</th>
+                            <th>Type</th>
+                            <th>Membership No.</th>
+                            <th>Form No</th>
+                            <th>Block Name</th>
+                            <th>Nominee Name</th>
+                            <th>Booking Name</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr class="align-middle">
-                            <td>1.</td>
+                            <td>1</td>
                             <td>Update software</td>
-                            <td>
-                                <div class="progress progress-xs">
-                                    <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                                </div>
-                            </td>
-                            <td><span class="badge text-bg-danger">55%</span></td>
-                        </tr>
-                        <tr class="align-middle">
-                            <td>2.</td>
-                            <td>Clean database</td>
-                            <td>
-                                <div class="progress progress-xs">
-                                    <div class="progress-bar text-bg-warning" style="width: 70%"></div>
-                                </div>
-                            </td>
-                            <td>
-                                <span class="badge text-bg-warning">70%</span>
-                            </td>
-                        </tr>
-                        <tr class="align-middle">
-                            <td>3.</td>
-                            <td>Cron job running</td>
-                            <td>
-                                <div class="progress progress-xs progress-striped active">
-                                    <div class="progress-bar text-bg-primary" style="width: 30%"></div>
-                                </div>
-                            </td>
-                            <td>
-                                <span class="badge text-bg-primary">30%</span>
-                            </td>
-                        </tr>
-                        <tr class="align-middle">
-                            <td>4.</td>
-                            <td>Fix and squish bugs</td>
-                            <td>
-                                <div class="progress progress-xs progress-striped active">
-                                    <div class="progress-bar text-bg-success" style="width: 90%"></div>
-                                </div>
-                            </td>
-                            <td>
-                                <span class="badge text-bg-success">90%</span>
-                            </td>
+                            <td>ad</td>
+                            <td>12345</td>
+                            <td>F-001</td>
+                            <td>A Block</td>
+                            <td>John Doe</td>
+                            <td>Arif Hussain</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-            <!-- /.card-body -->
+
+            <div class="card-footer clearfix">
+                <ul class="pagination pagination-sm m-0 float-end">
+                    <li class="page-item"><a class="page-link" href="#">«</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">»</a></li>
+                </ul>
+            </div>
         </div>
-        <!-- /.card -->
-
-
 
     </div>
 
