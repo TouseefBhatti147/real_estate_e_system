b1deb46e2e7b723ad1f87eddb965452287615a49
Arif Hussain <arif.hussain2014@gmail.com>
2025-11-21 18:45:55 +0000

    index page code revert

--- index.php
+++ index.php
@@ -33,8 +33,8 @@
     <!-- Header -->
     <?php require("includes/header.php"); ?>
 
-    <!-- Sidebar  client = bg-light-subtle, data-bs-theme= light , dark = bg-body-secondary , data-bs-theme= dark -->
-    <aside class="app-sidebar  bg-light-subtle shadow" data-bs-theme="light">
+    <!-- Sidebar -->
+    <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
       <div class="sidebar-brand">
         <a href="index.php" class="brand-link">
           <span class="brand-text fw-light">Real Estate E-System</span>
@@ -45,7 +45,6 @@
 
     <!-- Main Content -->
     <main class="app-main">
-
       <!--begin::App Content Header-->
       <div class="app-content-header">
         <div class="container-fluid">
@@ -68,14 +67,312 @@
       <div class="app-content">
         <div class="container-fluid">
 
-          <?php include("client-dashboard.php"); ?>
+          <!--start::Report Section-->
+          <div class="col-12">
+            <div class="callout callout-info">
+              Reports
+            </div>
+          </div>
+          <hr />
 
-          <!-- ?php include("admin-dashboard.php"); ?> -->
+          <div class="row">
+            <div class="col-md-3">
+              <a class="a" href="reports/plots_report.php">
+                <div class="info-box mb-3 text-bg-warning">
+                  <span class="info-box-icon"><i class="bi bi-tag-fill"></i></span>
+                  <div class="info-box-content">
+                    <span class="info-box-text">Plots Report</span>
+                    <span class="info-box-number">5,200</span>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-md-3">
+              <a class="a" href="reports/receipt_report.php">
+
+                <div class="info-box mb-3 text-bg-success">
+                  <span class="info-box-icon"><i class="bi bi-heart-fill"></i></span>
+                  <div class="info-box-content">
+                    <span class="info-box-text">Receipt Reports</span>
+                    <span class="info-box-number">92,050</span>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <!--end::Report Section-->
+
+
+          <!--start:Add New section-->
+          <div class="col-12 mt-4">
+            <div class="callout callout-info">
+              Add New
+            </div>
+          </div>
+          <hr />
+
+          <div class="row">
+            <a href="streets/streets.php">
+
+              <div class="col-md-3">
+                <div class="info-box mb-3 text-bg-primary">
+                  <span class="info-box-icon"><i class="bi bi-signpost-2-fill"></i></span>
+                  <div class="info-box-content">
+                    <span class="info-box-text">Streets</span>
+                  </div>
+                </div>
+            </a>
+          </div>
+
+
+
+          <div class="col-md-3"> <a href="projects/projects.php">
+              <div class="info-box mb-3 text-bg-warning">
+                <span class="info-box-icon"><i class="bi bi-building"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Projects</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+
+
+
+
+
+
+          <div class="col-md-3">
+            <a href="categories/categories.php">
+              <div class="info-box mb-3 text-bg-dark">
+                <span class="info-box-icon"><i class="bi bi-list-ul"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Categories List</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-md-3">
+            <a href="charges/charges.php">
+              <div class="info-box mb-3 text-bg-primary">
+                <span class="info-box-icon"><i class="bi bi-cash-coin"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Charges List</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-md-3">
+            <a href="plot_sizes/plot_size_list.php">
+              <div class="info-box mb-3 text-bg-secondary">
+                <span class="info-box-icon"><i class="bi bi-layout-text-sidebar-reverse"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Plot Size Categories</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-md-3">
+            <a href="installment_plan/installment_plan_list.php">
+
+              <div class="info-box mb-3 text-bg-warning">
+                <span class="info-box-icon"><i class="bi bi-calendar2-week"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Installment Plan</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-md-3">
+            <a href="sectors/sectors.php">
+              <div class="info-box mb-3 text-bg-success">
+                <span class="info-box-icon"><i class="bi bi-diagram-3-fill"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Sectors</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-md-3">
+            <a href="property_type/property_types.php">
+              <div class="info-box mb-3 text-bg-danger">
+                <span class="info-box-icon"><i class="bi bi-house-door-fill"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Property Type</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
 
         </div>
-      </div>
-      <!--end::App Content-->
+        <!--end:Add New section-->
+        <!--start:Media & Website section-->
+        <div class="col-12 mt-4">
+          <div class="callout callout-info">
+            Media & Website
+          </div>
+        </div>
+        <hr />
 
+        <div class="row">
+
+
+          <div class="col-md-3">
+            <a href="sliders/slider_list.php">
+
+              <div class="info-box mb-3 text-bg-warning">
+                <span class="info-box-icon"><i class="bi bi-sliders"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Slider</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-md-3">
+            <a href="sales_centers/sale_center_list.php">
+
+              <div class="info-box mb-3 text-bg-danger">
+                <span class="info-box-icon"><i class="bi bi-shop"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Sales Center</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+
+
+          <div class="col-md-3">
+            <a href="news/news_list.php">
+              <div class="info-box mb-3 text-bg-secondary">
+                <span class="info-box-icon"><i class="bi bi-newspaper"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">News</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+
+
+
+
+          <div class="col-md-3">
+            <a href="countries/country_list.php">
+              <div class="info-box mb-3 text-bg-danger">
+                <span class="info-box-icon"><i class="bi bi-globe-americas"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Manage Country</span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a href="cities/city_list.php">
+              <div class="info-box mb-3 text-bg-danger">
+                <span class="info-box-icon"><i class="bi bi-buildings"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Manage City</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+        </div>
+        <!--end:Media & Website section-->
+        <!--start:Users / Members section-->
+        <div class="col-12 mt-4">
+          <div class="callout callout-info">Users / Members</div>
+        </div>
+        <hr />
+
+        <div class="row">
+
+          <div class="col-md-3">
+            <a href="members/member_list.php">
+
+              <div class="info-box mb-3 text-bg-success">
+                <span class="info-box-icon"><i class="bi bi-people-fill"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Member's Directory</span>
+                  <span class="info-box-number">8,526</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-md-3">
+            <a href="users/user_list.php">
+
+              <div class="info-box mb-3 text-bg-danger">
+                <span class="info-box-icon"><i class="bi bi-person-fill"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">User</span>
+                  <span class="info-box-number">50</span>
+                </div>
+              </div>
+            </a>
+          </div>
+
+
+        </div>
+        <!--end:Users / Members section-->
+
+        <!--start:Security, Reporting, Balloting, Recovery section-->
+        <div class="col-12 mt-4">
+          <div class="callout callout-info">Security, Reporting, Balloting, Recovery</div>
+        </div>
+        <hr />
+
+        <div class="row">
+
+
+
+
+
+
+          <div class="col-md-3">
+            <div class="info-box mb-3 text-bg-warning">
+              <span class="info-box-icon"><i class="bi bi-wallet2"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">Finance System</span>
+              </div>
+            </div>
+          </div>
+
+
+          <!--end:Security, Reporting, Balloting, Recovery section-->
+
+          <!--start:Land and Society Map Management section-->
+          <div class="col-12 mt-4">
+            <div class="callout callout-info">Land and Society Map Management</div>
+          </div>
+          <hr />
+
+          <div class="row">
+            <div class="col-md-3">
+              <div class="info-box mb-3 text-bg-success">
+                <span class="info-box-icon"><i class="bi bi-map-fill"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">View Map</span>
+                </div>
+              </div>
+            </div>
+
+
+            <!--end:Land and Society Map Management section-->
+
+          </div>
+        </div>
+        <!--end::App Content-->
     </main>
 
 
@@ -91,36 +388,49 @@
 
   <!-- ApexCharts -->
   <script src="./js/apexcharts.min.js" crossorigin="anonymous"></script>
-  <!-- <script>
-      const sales_chart_options = {
-        series: [
-          { name: 'Digital Goods', data: [28, 48, 40, 19, 86, 27, 90] },
-          { name: 'Electronics', data: [65, 59, 80, 81, 56, 55, 40] }
-        ],
-        chart: {
-          height: 280,
-          type: 'area',
-          toolbar: { show: false }
+  <script>
+    const sales_chart_options = {
+      series: [{
+          name: 'Digital Goods',
+          data: [28, 48, 40, 19, 86, 27, 90]
         },
-        colors: ['#0d6efd', '#20c997'],
-        dataLabels: { enabled: false },
-        stroke: { curve: 'smooth' },
-        xaxis: {
-          type: 'datetime',
-          categories: [
-            '2023-01-01', '2023-02-01', '2023-03-01',
-            '2023-04-01', '2023-05-01', '2023-06-01',
-            '2023-07-01'
-          ]
-        },
-        tooltip: {
-          x: { format: 'MMMM yyyy' }
+        {
+          name: 'Electronics',
+          data: [65, 59, 80, 81, 56, 55, 40]
         }
-      };
+      ],
+      chart: {
+        height: 280,
+        type: 'area',
+        toolbar: {
+          show: false
+        }
+      },
+      colors: ['#0d6efd', '#20c997'],
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'smooth'
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: [
+          '2023-01-01', '2023-02-01', '2023-03-01',
+          '2023-04-01', '2023-05-01', '2023-06-01',
+          '2023-07-01'
+        ]
+      },
+      tooltip: {
+        x: {
+          format: 'MMMM yyyy'
+        }
+      }
+    };
 
-      const sales_chart = new ApexCharts(document.querySelector('#sales-chart'), sales_chart_options);
-      sales_chart.render();
-    </script> -->
+    const sales_chart = new ApexCharts(document.querySelector('#sales-chart'), sales_chart_options);
+    sales_chart.render();
+  </script>
 </body>
 
 </html>
